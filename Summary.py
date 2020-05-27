94994c491d77537aeb100eeba76b4908eec5dea0
haohoang <50910747+haohoang@users.noreply.github.com>
2020-05-28 00:24:44 +0700

    Add files via upload

---
+++ Summary.py
@@ -0,0 +1,191 @@
+import numpy as np
+import pandas as pd
+import heapq
+import util
+from sklearn.cluster import Kmeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
+import logging
+logging.basicConfig(format='%(filename)s: %(message)s',
+                    level=logging.INFO)
+
+
+class Summary():
+    """
+    Attribute:
+        - data: list of list - article, position, raw_text
+        - df: DataFrame with row: sentence, column = "article, position, raw_text"
+            article: own sentence
+            position of the sentence in the article
+            raw_text: sentence before preprocess
+        - model: word embedding
+        - rel_position: True if sort sentences in final summary based on average of 
+                      all sentences' position in a cluster
+                      Default: True
+        - n_clusters: number of clusters in Kmeans
+        - lsa: True if use LSA after Kmean, default=False
+        - mmr: True if use MMR after LSA or Kmeans, default=False
+        - random_state: default = 42
+        - lamda: only used when mmr is used, default = 0.6
+        - n_component: only use when lsa is True, default=20
+        - n_iter: only use when lsa is True. default = 10
+    """
+    def __init__(self, data=None, model=None, rel_position=True, n_clusters=16, lsa=False, mmr=False, 
+                 random_state= 42, lamda=0.6, n_components=20, n_iter=10):
+        self.df = pd.DataFrame(data, columns= ["article", "position", "raw_text"])
+        self.model = model
+        self.rel_position = rel_position
+        self.n_clusters = n_clusters
+        self.lsa = lsa
+        self.mmr = mmr
+        self.random_state = 42
+        self.lamda =lamda
+        self.n_components=n_components
+        self.n_iter = n_iter
+        
+    def clean_sent(self):
+        logging.info("Start cleaning sentences")
+        self.df["clean_text"] = ""
+        for index, row in self.df.iterrows():
+          if index % 10 == 0:
+              logging.info("Cleaned " + index + "sentences")
+          temp = util.clean_not_remain_tag(row["raw_text"])
+          if len(temp) == 0:
+            logging.info("Delete sentence" + index + "because of zero length")
+            self.df.drop(labels=[index], inplace=True)
+          else:
+            self.df.at[index, "clean_text"] = temp
+        logging.info("Finish cleaning sentences")
+        logging.indo("Reindex ")
+        self.df.index = range(len(self.df))
+        
+    def sent2vec(self):
+        logging.info("Start computing vectors")
+        self.df["vector"] = None
+        for index, row in self.df.iterrows():
+            if index % 10 == 0:
+              logging.info("Computed " + index + "vector sentences")
+            self.df.at[index, "vector"] = util.compute_vector(self.model, row["raw_text"])
+        logging.info("Finishing compute vectors")    
+            
+    def trainKmean(self):
+        self.clean_sent()
+        self.sent2vec()
+        self.kmean = Kmeans(n_clusters=self.n_clusters, random_state=self.random_state)
+        self.kmean.fit(self.df['vector'].tolist())
+        self.df["label"] = self.kmean.labels_
+        self.choose_centers()
+        self.kmean_summary()
+        
+    def choose_centers(self):
+        logging.info("Choosing center in each cluster")
+        self.df['choose'] = False
+        self.relative_position = list()
+        for i in range(self.n_clusters):
+            index = self.df['label'] == i
+            center = self.kmean.cluster_centers_[i]
+            minDistance = self.kmean.inertia_
+            centeri = None
+            for idx, row in self.df[index].iterrows():
+                if np.linalg.norm(row['vector'] - center) <= minDistance:
+                    centeri = idx
+            self.df.at[centeri, 'choose'] = True
+            if self.rel_position:
+                self.relative_position.append(self.df[index]['position'].mean())
+        logging.info("Finish choosing")    
+
+    def kmean_summary(self):
+        index = self.df['choose'] == True
+        self.summary = self.df[index].sort_values(['label'])
+        if self.rel_position:
+            self.summary['relative_position'] = np.array(self.relative_position)
+        self.summary = self.summary.sort_values(['position'])
+        self.summary.index = range(len(self.n_clusters))
+        
+    def build_vocab(self):
+        corpus = self.summary['clean_text'].tolist()
+        for idx, sent in enumerate(corpus):
+            corpus[idx] = " ".join(sent)
+        return corpus
+    
+    def LSA(self):
+        corpus = self.build_vocab()
+        vectorizer = TfidfVectorizer(use_idf=True, smooth_idf=True)
+        X = vectorizer.fit_transform(corpus)
+        svd = TruncatedSVD(n_components=self.n_components, n_iter=self.n_iter, random_state=self.random_state)
+        Y = svd.fit_transform(X)
+        A = Y.mean(axis=1)
+        index = heapq.nlargest(self.n_components, range(len(A)), A.take)
+        return index 
+        
+    def MMR(self):
+        if self.lsa:
+            index = self.LSA()
+            self.summary = self.summary.iloc[index]
+            self.summary.index = range(self.n_components)
+        self.summary['choose'] = 0
+        
+        # choose first sentence for final summary
+        S = self.summary[['position']].idxmin()
+        first_sent = S.get(key='position')
+        self.summary.at[first_sent, 'choose'] = 1
+        
+        # compute tf-idf
+        corpus = self.build_vocab()
+        vectorizer = TfidfVectorizer(use_idf=True, smooth_idf=True)
+        X = vectorizer.fit_transform(corpus)
+        self.idf = pd.DataFrame(vectorizer.idf_, index=vectorizer.get_feature_names())
+        count = CountVectorizer()
+        X = count.fit_transform(corpus)
+        self.tf = pd.DataFrame(X.toarray(), columns=count.get_feature_names())
+        
+        # ranking
+        for i in range(15):
+            max_sim = -100000000
+            max_idx = -1
+            S1 = self.summary['choose'] == 0
+            S2 = self.summary['choose'] == 2
+            for idx_v, row in self.summary[S1].iterrows():
+                sim1 = util.sim(first_sent, idx_v)
+                max_sim2 = 0
+                if i > 0:
+                    for idx_u, row_u in self.summary[S2].iterrows():
+                        sim2 = util.sim(idx_u, idx_v)
+                        max_sim2 = max(max_sim2, sim2)
+                simV = self.lamda*(sim1 - (1-self.lamda)*sim2)
+                if simV >= max_sim:
+                    max_sim = simV
+                    max_idx = idx_v
+            self.summary[max_idx, 'choose'] = 2
+        
+        self.summary.at[first_sent, 'choose'] = 2
+        index = self.summary['choose'] == 2
+        self.summary = self.summary[index]
+        
+    def sim(self,idx_u, idx_v):
+      """ idx_u: index of sentence u
+          idx_v: index of sentence v
+      Compute similarity between sentence u and sentence v"""
+      tu_so = 0
+      mau_so = 0
+      for word in self.summary.at[idx_v, 'clean_text']:
+        tu_so += self.tf.at[idx_u, word] * self.tf.at[idx_v, word] * (self.idf.at[word, 0] ** 2)
+      for word in self.summary.at[idx_u, 'clean_text']:
+        mau_so += (self.tf.at[idx_u, word] * self.idf.at[word, 0]) ** 2
+      mau_so = mau_so ** 0.5
+      return tu_so / mau_so
+  
+    def save_summary(self, filename):
+        tom_tat = self.summary['raw_text'].tolist()
+        with open(filename, 'w') as f:
+            for sent in tom_tat:
+                f.write(sent + "\n")
+        
+    def summary(self, filename):
+        self.trainKmean()
+        if self.mmr:
+            self.MMR()
+        self.save_summary(filename)
+        
+        
---
+++ util.py
@@ -0,0 +1,74 @@
+import nltk
+import numpy as np
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from string import punctuation
+from collections import defaultdict
+from gensim.models import FastText
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+p = punctuation + "0123456789"
+stop = set(stopwords.words('english'))
+exclude = set(p)
+
+
+def nltk_tag_to_wordnet_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+
+
+def lemmatize_sentence_not_retain_tag(sentence):
+    lemmatizer = WordNetLemmatizer()
+    # tokenize the sentence and find the POS tag for each token
+    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))
+    # tuple of (token, wordnet_tag)
+    wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
+    lemmatized_sentence = []
+    for word, tag in wordnet_tagged:
+        if tag is None:
+            # if there is no available tag, append the token as is
+            lemmatized_sentence.append(word.lower())
+        else:
+            # else use the tag to lemmatize the token
+            lemmatized_sentence.append(lemmatizer.lemmatize(word.lower(), tag))
+    return lemmatized_sentence
+
+
+def clean_not_remain_tag(doc):
+    # remove stop words, punctuation, and lemmatize words
+    lemm = lemmatize_sentence_not_retain_tag(doc)
+    s_free = " ".join([i for i in lemm if i not in stop])
+    p_free = ''.join([ch for ch in s_free if ch not in exclude])
+    # only take words which are greater than 2 characters"
+    cleaned = [word for word in p_free.split() if len(word) >= 2]
+    return cleaned
+
+def compute_vector(model, sent):
+    """model: word embedding
+       sent: list of words
+       return vector of sent"""
+  vector = np.zeros(model.vector_size)
+  for word in sent:
+    vector += model[word]
+  return vector
+
+
+if __name__ == "__main__":
+    import json
+    with open("data5.json") as f:
+        data = json.load(f)
+    topic = Topic(topic="D0701A", data=data)
+    print(topic.topic)
